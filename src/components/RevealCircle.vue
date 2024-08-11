a2b9938fd746028cdb178ae3100f296a2bada656
sahidAle <sahidaledev@gmail.com>
2024-08-11 15:40:54 -0600

    feat(reveal-circle): agregar lógica para mostrar diálogo de confirmación y ajustar visualmente datos del pomodoro

--- src/components/RevealCircle.vue
+++ src/components/RevealCircle.vue
@@ -2,21 +2,22 @@
 import { ref } from 'vue'
 import { usePomodoroStore } from '@/stores/pomodoro';
 import type { StatusSession } from '@/stores/pomodoro';
+import useService from '@/services';
 
 /**
  * ------------------------------------------
  *	Components
  * ------------------------------------------
  */
-import IconCheck from '@/components/icons/CircleCheck.vue'
 import FireIcon from './icons/FireIcon.vue';
+import AngleLeftIcon from './icons/AngleLeftIcon.vue';
+import DialogConfirm from './widgets/DialogConfirm.vue';
 
 /**
  * ------------------------------------------
  *	Utils
  * ------------------------------------------
  */
-
 interface Props {
   mainText: string
   secondaryText: string
@@ -29,20 +30,21 @@ interface Emit {
 
 defineProps<Props>()
 const $emit = defineEmits<Emit>()
+const $service = useService()
 const $pomodoro = usePomodoroStore()
+
 /**
  * ------------------------------------------
  *	Data
  * ------------------------------------------
  */
-
 const transition = ref('circle-enter-active')
-
 const classByStatus: Record<StatusSession, string> = {
   'completed': 'text-[#FC5185]',
   'current': 'text-[#3FC1C9]',
   'pending': 'text-[#FC5185]/20'
 }
+const showDialog = ref<boolean>(false)
 
 /**
  * ------------------------------------------
@@ -54,11 +56,46 @@ const classByStatus: Record<StatusSession, string> = {
  * handleClickTransition
  *
  */
-
 const handleClickTransition = () => {
   transition.value = 'circle-leave-active'
   $pomodoro.session = null
   $emit('action:cancel')
+}
+
+/**
+ * handleDialog
+ *
+ */
+const handleDialog = () => {
+  showDialog.value = true
+}
+
+/**
+ * leaveWithoutSave
+ *
+ */
+ const leaveWithoutSave = () => {
+  console.log('Leave without save')
+  $pomodoro.clearSession()
+  showDialog.value = false
+  handleClickTransition()
+}
+
+/**
+ * saveProgress
+ *
+ */
+const saveProgress = async () => {
+  try {
+    if(!$pomodoro.session) return
+  
+    const reponse = await $service.updatePomodoroSession($pomodoro.session._id,$pomodoro.session)
+    console.log(reponse)
+    console.log('Save progress')
+
+  } catch (error) {
+    console.error(error)    
+  }
 }
 </script>
 
@@ -66,54 +103,61 @@ const handleClickTransition = () => {
   <transition name="circle">
     <div v-if="true"  class="fixed z-10 top-0 bottom-0 left-0 right-0 bg-slate-950">
       <div class="min-h-screen bg-[radial-gradient(circle_500px_at_50%_200px,#3e3e3e,transparent)]">
-        <section>
-          <div class="flex justify-end p-4">
-            <button @click="handleClickTransition">
-              <IconCheck class="text-white" />
-            </button>
-          </div>
-        </section>
 
-        <section class="px-10 py-32 text-white text-center space-y-8">
-          <div class="flex justify-center items-center gap-4">
-            <div 
-              class="flex justify-center items-center text-5xl border rounded-lg w-32 h-32"
-              :class="[$pomodoro.getIsActive && $pomodoro.getIsWorkCycle ? 'border-[#3FC1C9]' : 'border-white']"
-            >
-              {{  $pomodoro.minutes }}
+        <DialogConfirm v-if="showDialog" @action:close="showDialog=false" @action:not-save="leaveWithoutSave"  />
+
+        <div class="px-10 py-10">
+          <section class="flex items-center gap-4 mb-20 text-white cursor-pointer">
+
+            <AngleLeftIcon @click="handleDialog" class="size-6 hover:bg-[#364F6B] rounded-sm transition" />
+            Regresar
+          </section>
+  
+          <section class="text-white text-center space-y-8 max-w-lg mx-auto">
+            <div class="flex justify-center items-center gap-4">
+              <div 
+                class="flex justify-center items-center text-5xl border rounded-lg w-32 h-32"
+                :class="[$pomodoro.getIsActive && $pomodoro.getIsWorkCycle ? 'border-[#3FC1C9]' : 'border-white']"
+              >
+                {{  $pomodoro.minutes }}
+              </div>
+              :
+              <div 
+                class="flex justify-center items-center text-5xl border rounded-lg w-32 h-32"
+                :class="[$pomodoro.getIsActive && $pomodoro.getIsWorkCycle  ? 'border-[#3FC1C9]' : 'border-white']"
+              >
+                {{ $pomodoro.seconds }}
+              </div>
             </div>
-            :
-            <div 
-              class="flex justify-center items-center text-5xl border rounded-lg w-32 h-32"
-              :class="[$pomodoro.getIsActive && $pomodoro.getIsWorkCycle  ? 'border-[#3FC1C9]' : 'border-white']"
-            >
-              {{ $pomodoro.seconds }}
+  
+            <h2 class="text-2xl ">
+              {{ $pomodoro.session?.title }}
+            </h2>
+  
+  
+            <div class="space-y-2">
+              <h3 class="text-xl" :class="[ $pomodoro.getIsWorkCycle ? 'text-[#3FC1C9]' : 'text-white' ]">
+                Sesión de {{ $pomodoro.getIsWorkCycle ? 'Trabajo' : 'Descanso' }}
+              </h3>
+    
+              <div class="flex justify-center items-center">
+                <FireIcon v-for="(status, index) in $pomodoro.cycleStatus()" :key="index" class="size-8"  :class="classByStatus[status]" />
+              </div>
             </div>
-          </div>
-
-          <h2 class="text-2xl ">
-            {{ $pomodoro.session?.title }}
-          </h2>
-
-          <h3 class="text-xl">
-            Sesión de {{ $pomodoro.getIsWorkCycle ? 'Trabajo' : 'Descanso' }}
-          </h3>
-
-          <div class="flex justify-center items-center">
-            <FireIcon v-for="(status, index) in $pomodoro.cycleStatus()" :key="index" :class="classByStatus[status]" />
-          </div>
-
-          <div>
-            <button v-if="$pomodoro.getIsActive" @click="$pomodoro.pauseSession()">
+  
+            <button v-if="$pomodoro.getIsActive" @click="$pomodoro.pauseSession()" class="btn-medium btn-primary w-36">
               Pausar
             </button>
 
-            <button v-else @click="$pomodoro.resumeSession()">
-              Reanudar
-            </button>
-          </div>
+            <div v-else class="flex justify-center gap-4">
+              <button class="btn-medium btn-primary w-36" @click="$pomodoro.resumeSession()">
+                Reanudar
+              </button>
 
-        </section>
+              <button class="btn-medium btn-secondary" @click="saveProgress">Guardar progreso</button>
+            </div>
+          </section>
+        </div>
       </div>
     </div>
   </transition>
--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,13 @@
 import './assets/main.css'
-
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-import VueDatePicker from '@vuepic/vue-datepicker';
 import '@vuepic/vue-datepicker/dist/main.css'
 
+import { autoAnimatePlugin } from '@formkit/auto-animate/vue'
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import AlertNotification from '@/components/AlertNotification.vue'
 import App from './App.vue'
 import router from './router'
-
-import { autoAnimatePlugin } from '@formkit/auto-animate/vue'
+import VueDatePicker from '@vuepic/vue-datepicker';
 
 const app = createApp(App)
 
@@ -16,5 +15,6 @@ app.use(autoAnimatePlugin)
 app.use(createPinia())
 app.component('VueDatePicker', VueDatePicker)
 app.use(router)
+app.component('AlertNotification', AlertNotification)
 
 app.mount('#app')
