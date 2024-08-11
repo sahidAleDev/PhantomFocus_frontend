66871f8cc579422bc1c056dd9201bbe0bee9aab2
sahidAle <sahidaledev@gmail.com>
2024-08-11 15:17:18 -0600

    feat(dialogos): integrar DialogConfirm.vue y modificar DialogPomodoro.vue
    - Integrado el nuevo componente DialogConfirm.vue para la gestión de confirmaciones.
    - Modificado el componente DialogPomodoro.vue para mejorar la interacción con las nuevas confirmaciones.

---
+++ src/components/widgets/DialogConfirm.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+
+/**
+ * ------------------------------------------
+ *	Utils
+ * ------------------------------------------
+ */
+
+interface Emits {
+  (event: 'action:not-save'): void
+  (event: 'action:close'): void
+  
+}
+const $emits = defineEmits<Emits>()
+
+
+</script>
+
+<template>
+  <article id="container_popup" class="fixed z-20 flex justify-center items-center h-screen w-full top-0 left-0 right-0 bottom-0 px-4 bg-black/40">
+    <section id="popup" class="w-full sm:max-w-sm bg-white rounded-2xl popUp_fadeIn p-4 space-y-4">
+      <p class="font-semibold text-lg sm:text-xl ">¿Estás seguro de que quieres salir? Tú progreso se perderán</p>
+
+
+      <div class="flex gap-4">
+        <button @click="$emits('action:not-save')" class="btn-primary-outline btn-small w-full">Salir sin guardar</button>
+        <button @click="$emits('action:close')" class="btn-primary btn-small w-full">Permanecer</button>
+      </div>
+    </section>
+  </article>
+</template>
